5c7e4f094c111f9b5fe420b607c3caf2c627e50d
CakeOrange <gnehc.gao@mail.utoronto.ca>
2020-03-16 15:38:01 -0400

    test

--- frontend/src/components/utilities/uploadImage.js
+++ frontend/src/components/utilities/uploadImage.js
@@ -12,57 +12,6 @@ import { InboxOutlined } from '@ant-design/icons';
 import TextArea from 'antd/lib/input/TextArea';
 
 
-// function getBase64(img, callback) {
-//     const reader = new FileReader();
-//     reader.addEventListener('load', () => callback(reader.result));
-//     reader.readAsDataURL(img);
-//   }
-
-function formDataToBuffer( formData ) {
-    let dataBuffer = new Buffer( 0 );
-    let boundary   = formData.getBoundary();
-    for( let i = 0, len = formData._streams.length; i < len; i++ ) {
-
-        if( typeof formData._streams[i] !== 'function' ) {
-
-            dataBuffer = this.bufferWrite( dataBuffer, formData._streams[i] );
-
-            // The item have 2 more "-" in the boundary. No clue why
-            // rfc7578 specifies (4.1): "The boundary is supplied as a "boundary"
-            //    parameter to the multipart/form-data type.  As noted in Section 5.1
-            //    of [RFC2046], the boundary delimiter MUST NOT appear inside any of
-            //    the encapsulated parts, and it is often necessary to enclose the
-            //    "boundary" parameter values in quotes in the Content-Type header
-            //    field."
-            // This means, that we can use the boundary as unique value, indicating that
-            // we do NOT need to add a break (\r\n). These are added by data-form package.
-            //
-            // As seen in this example (https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/POST#Example)
-            // the boundary is preceded by 2x "-". If thus --Boundary exists, do not add the break.
-            if( typeof formData._streams[i] !== 'string' || formData._streams[i].substring( 2, boundary.length + 2 ) !== boundary ) {
-                dataBuffer = bufferWrite( dataBuffer, "\r\n" );
-            }
-        }
-    }
-
-    // Close the request
-    dataBuffer = bufferWrite( dataBuffer, '--' + boundary + '--' );
-
-    return dataBuffer;
-}
-
-function bufferWrite( buffer, data ) {
-
-    let addBuffer;
-    if( typeof data === 'string' ) {
-        addBuffer = Buffer.from( data );
-    }
-    else if( typeof data === 'object' && Buffer.isBuffer( data ) ) {
-        addBuffer = data;
-    }
-
-    return Buffer.concat( [buffer, addBuffer] );
-}
 
 
 const dummyRequest = ({ file, onSuccess }) => {
@@ -121,13 +70,15 @@ class UploadImage extends Component{
             description: '',
             fileList: [],
             file: null,
-            // location: {
-            //     latitude: null,
-            //     longtitude: null,
-            // },
+            checked: false,
+            location: {
+                latitude: null,
+                longtitude: null,
+            },
         };
         // this.handleSubmit = this.handleSubmit.bind(this);
         this.customSubmit = this.customSubmit.bind(this);
+        this.handleSwitch = this.handleSwitch.bind(this);
 
         
     }
@@ -170,6 +121,24 @@ class UploadImage extends Component{
         }
     }
 
+    handleSwitch = (e) => {
+        console.log("before "+this.state.checked)
+        if (this.state.checked){
+            let loc = {
+                latitude: null,
+                longtitude: null
+            }
+            this.setState({
+                location: loc,
+                checked: !this.state.checked,
+            })
+        }
+        this.setState({
+            checked: !this.state.checked,
+        })
+        console.log("after "+this.state.checked)
+    }
+
     // handleSubmit = (e) => {
     //     e.preventDefault();
     //     let image = {
@@ -210,8 +179,7 @@ class UploadImage extends Component{
         axios
             .post(API_END_POINT+'/api/images', data, config)
             .then((res) => {
-                console.log("1 "+res.data)
-                console.log("2 "+this.state.file)
+                console.log(res)
         }).catch((err) => {
             console.log(err)
         })
@@ -286,9 +254,13 @@ class UploadImage extends Component{
                     onChange={this.handleChange}/>
                 </Form.Item>
 
-                <Form.Item name="useLocation" label="Show Location" valuePropName="checked">
+                <Form.Item 
+                    name="useLocation" 
+                    label="Show Location" 
+                    valuePropName="checked">
                     <Switch 
-                    name="Location"/>
+                    name="Location"
+                    onChange={this.handleSwitch}/>
                 </Form.Item>
 
                 <Form.Item
