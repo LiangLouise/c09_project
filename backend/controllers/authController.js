3c7abdb12fc98df860c8385f9389616104bc64a2
LiangLouise <liangroy@yahoo.com>
2020-04-07 22:42:41 -0400

    Update Auth Part API Doc

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -12,7 +12,9 @@ const logger = require('../config/loggerconfig');
  * @apiExample {curl} Example Usage:
  *  curl -H "Content-Type: application/json" -X POST -d '{"username":"alice","password":"alice"}' -c cookie.txt localhost:5000/signup/
  *
- * @apiParam {String} username New User Username.
+ * @apiHeader {String} Content-Type Only Accept `application/json`.
+ *
+ * @apiParam {String} username New User Username, must be Alphanumeric.
  * @apiParam {String} password New User Password.
  *
  * @apiSuccess {String} success user {username} signed up
@@ -20,9 +22,10 @@ const logger = require('../config/loggerconfig');
  * @apiSuccessExample {json} Success-Response:
  *     HTTP/1.1 200 OK
  *     {
- *       "user {username} signed up"
+ *       "success": "user {username} signed up"
  *     }
  *
+ * @apiError (Error 400) BadFormat Username is not Alphanumeric
  * @apiError (Error 409) UsernameUsed The Username has been used by others.
  * @apiError (Error 500) InternalServerError Error Message from backend.
  */
@@ -33,13 +36,13 @@ exports.signup = function(req, res, next) {
     db.users.findOne({_id: username}, function (err, user) {
         if (err) {
             logger.error(err);
-            return res.status(500).end();
+            return res.status(500).end(err);
         }
         if (user) return res.status(409).end("username " + username + " already exists");
         db.users.insert(new User(username, password), function (err, user) {
             if (err) {
                 logger.error(err);
-                return res.status(500).end();
+                return res.status(500).end(err);
             }
             return res.json({
                 success: "user " + username + " signed up"
@@ -48,7 +51,31 @@ exports.signup = function(req, res, next) {
     });
 };
 
-// curl -H "Content-Type: application/json" -X POST -d '{"username":"alice","password":"alice"}' -c cookie.txt localhost:5000/signin/
+/**
+ * @api {post} /signin User Sign In
+ * @apiName Sign In
+ * @apiGroup Auth
+ *
+ * @apiExample {curl} Example Usage:
+ *  curl -H "Content-Type: application/json" -X POST -d '{"username":"alice","password":"alice"}' -c cookie.txt localhost:5000/signin/
+ *
+ * @apiHeader {String} Content-Type Only Accept `application/json`.
+ *
+ * @apiParam {String} username Username of the user to sign in, must be Alphanumeric.
+ * @apiParam {String} password Password of the user to sign in.
+ *
+ * @apiSuccess {String} success user {username} signed in
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "success": "user {username} signed up"
+ *     }
+ *
+ * @apiError (Error 400) BadFormat Username is not Alphanumeric
+ * @apiError (Error 401) AccessDeny Wrong Username/Password.
+ * @apiError (Error 500) InternalServerError Error Message from backend.
+ */
 exports.signin = function (req, res, next) {
     let username = req.body.username;
     let password = req.body.password;
@@ -56,7 +83,7 @@ exports.signin = function (req, res, next) {
     db.users.findOne({_id: username}, function (err, user) {
         if (err) {
             logger.error(err);
-            return res.status(500).end();
+            return res.status(500).end(err);
         }
         if (!user) return res.status(401).end("access denied");
         if (user.hash !== generateHash(password, user.salt)) return res.status(401).end("access denied");
@@ -66,13 +93,34 @@ exports.signin = function (req, res, next) {
 
         logger.info("Login response header", res.getHeaders());
 
-        return res.json("user " + username + " signed in");
+        return res.json({
+            success: "user " + username + " signed in"
+        });
     });
 };
 
-// curl -b cookie.txt -c cookie.txt localhost:3000/signout/
+/**
+ * @api {get} /signout User Sign Out
+ * @apiName Sign In
+ * @apiGroup Auth
+ *
+ * @apiExample {curl} Example Usage:
+ *  curl -b cookie.txt -c cookie.txt localhost:3000/signout/
+ *
+ * @apiSuccess {String} success user {username} signed out
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "success": "user {username} signed out"
+ *     }
+ *
+ */
 exports.signout = function (req, res, next) {
+    let username = req.session.username;
     req.session.destroy();
     res.setHeader('Set-Cookie', c_configs.cookie.serialize('username', '', c_configs.cookie_config));
-    return res.status(200).end();
+    return res.json({
+        success: "user " + username + " signed out"
+    });
 };
