3865eeddc85d60f014ca5c27e840bafaa7762462
LiangLouise <liangroy@yahoo.com>
2020-04-09 19:48:14 -0400

    Add time field to comment

--- backend/model/comment.js
+++ backend/model/comment.js
@@ -2,6 +2,7 @@ let Comment= function (req, post_id) {
   this.post_id = post_id;
   this.username = req.session.username;
   this.content = req.body.content;
+  this.time =  Date.now();
 };
 
 module.exports = Comment;
